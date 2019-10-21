5554a1f4d255ae47121b9b714a36bfb53ba1e947
Yashas Gautham <ygautham001@nam.ad.pwcinternal.com>
2019-10-21 19:41:57 +0530

    checked in the Pacman code

---
+++ Models/Pacman.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacmanSimulator.Models
+{
+    public static class Pacman
+    {
+        static int xPos;
+        static int yPos;
+        static Direction direction;
+        static bool IsPlaced = false;
+
+
+        public static void Place(int x, int y, string direct)
+        {
+            if (x < 0 || x > 4 || y < 0 || y > 4 || (!direct.ToUpper().Trim().Equals(Direction.NORTH.ToString()) &&
+                !direct.ToUpper().Trim().Equals(Direction.EAST.ToString()) &&
+                !direct.ToUpper().Trim().Equals(Direction.SOUTH.ToString()) &&
+                !direct.ToUpper().Trim().Equals(Direction.WEST.ToString())))
+            {
+                Console.WriteLine("Pacman cannot be placed at desired position OR Invalid command format. Expected format : PLACE x,y,'NORTH/SOUTH/EAST/WEST'");
+            }
+            else
+            {
+                xPos = x;
+                yPos = y;
+                direction = (Direction)Enum.Parse(typeof(Direction), direct, true);
+                IsPlaced = true;
+            }
+        }
+        public static void Move()
+        {
+            if (!IsPlaced)
+            {
+                Console.WriteLine("You need to PLACE the pacman before anything!");
+            }
+            else
+            {
+                if (direction == Direction.NORTH && (yPos + 1) <= 4) yPos++;
+                else if (direction == Direction.SOUTH && (yPos - 1) >= 0) yPos--;
+                else if (direction == Direction.EAST && (xPos + 1) <= 4) xPos++;
+                else if (direction == Direction.WEST && (xPos - 1) >= 0) xPos--;
+                else
+                {
+                    Console.WriteLine("Pacamn should not fall off the grid.");
+                }
+            }
+        }
+        public static void Left()
+        {
+            if (!IsPlaced)
+            {
+                Console.WriteLine("You need to PLACE the pacman before anything!");
+            }
+            else
+            {
+
+                int facing = (int)direction;
+                facing--;
+                if (facing < 0)
+                    direction = Direction.WEST;
+                else
+                    direction = (Direction)facing;
+            }
+        }
+        public static void Right()
+        {
+            if (!IsPlaced)
+            {
+                Console.WriteLine("You need to PLACE the pacman before anything!");
+            }
+            else
+            {
+
+                int facing = (int)direction;
+                facing++;
+                if (facing > 3)
+                    direction = Direction.NORTH;
+                else
+                    direction = (Direction)facing;
+            }
+        }
+        public static void Report()
+        {
+            if (!IsPlaced)
+            {
+                Console.WriteLine("You need to PLACE the pacman before anything!");
+            }
+            else
+            {
+                Console.WriteLine("{0},{1},{2}", xPos, yPos, direction.ToString());
+            }
+        }
+
+        enum Direction
+        {
+            NORTH = 0,
+            EAST = 1,
+            SOUTH = 2,
+            WEST = 3
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using PacmanSimulator.Models;
+
+namespace PacmanSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Place the Pacman please.");
+            string command;
+            do
+            {
+                command = Console.ReadLine();
+                if (command.ToUpper().Contains("PLACE"))
+                {
+                    PlacePacman(command);
+                }
+                else if(command.ToUpper().Contains("MOVE") || command.ToUpper().Contains("LEFT") || command.ToUpper().Contains("RIGHT") ||
+                    command.ToUpper().Contains("REPORT"))
+                {
+                    Command(command);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid command.");
+                }
+                Console.WriteLine("Enter a command or ENTER to exit.");
+            } while (!string.IsNullOrWhiteSpace(command));
+        }
+        static void PlacePacman(string command)
+        {
+            string[] splitCmd = command.Split(new char[] { ',', ' ' });
+            int xVal;
+            int yVal;
+            if (splitCmd.Length == 4 && splitCmd[0].ToUpper().Trim().Equals("PLACE") && int.TryParse(splitCmd[1], out xVal) && int.TryParse(splitCmd[2], out yVal))
+            {
+                //[0] - PLACE
+                //[1] - x
+                //[2] - y
+                //[3] - NORTH/SOUTH/EAST/WEST
+
+                Pacman.Place(xVal, yVal, splitCmd[3]);
+                return;
+            }
+            Console.WriteLine("Invalid command format. Expected format : PLACE x,y,'NORTH/SOUTH/EAST/WEST'");
+
+        }
+        static void Command(string command)
+        {
+            if (command.Trim().ToUpper().Equals("MOVE")) { Pacman.Move(); }
+            else if (command.Trim().ToUpper().Equals("LEFT")) { Pacman.Left(); }
+            else if (command.Trim().ToUpper().Equals("RIGHT")) { Pacman.Right(); }
+            else if (command.Trim().ToUpper().Equals("REPORT")) { Pacman.Report(); }
+            else Console.WriteLine("Invalid command");
+        }
+    }
+}
